8d66142c09dcbe74518f14f10be88595238e3da5
Maks <maks135254@gmail.com>
2024-03-02 02:59:08 +0200

    update

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -4,10 +4,10 @@ export const createContactSchema = Joi.object({
     name: Joi.string().min(1).required(),
     email: Joi.string().email().required(),
     phone: Joi.string().min(6).required()
-})
+});
 
 export const updateContactSchema = Joi.object({
     name: Joi.string().min(1),
     email: Joi.string().email(),
     phone: Joi.string().min(6)
-})
+});
