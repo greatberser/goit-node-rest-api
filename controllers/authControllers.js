896e0cf4d48836d5d5588586d015c92b80103801
Maks <maks135254@gmail.com>
2024-03-26 11:59:17 +0200

    added controllers, services for auth

---
+++ controllers/authControllers.js
@@ -0,0 +1,89 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+import * as authServices from "../services/authServices.js";
+import * as userServices from "../services/userServices.js";
+
+import HttpError from "../helpers/HttpError.js";
+
+const { JWT_SECRET } = process.env;
+
+const signup = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    const user = await userServices.findUser({ email });
+    if (user) {
+      throw HttpError(409, "Email already in use");
+    }
+
+    const newUser = await authServices.signup(req.body);
+
+    res.status(201).json({
+      email: newUser.email,
+      subscription: newUser.subscription,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const signin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userServices.findUser({ email });
+    if (!user) {
+      throw HttpError(401, "Email or password invalid"); // "Email invalid"
+    }
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+      throw HttpError(401, "Email or password invalid"); // "Password invalid"
+    }
+
+    const payload = {
+      id: user._id,
+    };
+
+    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
+    await authServices.setToken(user._id, token);
+
+    res.json({
+      token,
+      user: { email: user.email, subscription: user.subscription },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getCurrent = async (req, res, next) => {
+  try {
+    const { email, subscription } = req.user;
+
+    res.json({
+      email,
+      subscription,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const signout = async (req, res, next) => {
+  try {
+    const { _id } = req.user;
+    await authServices.setToken(_id);
+
+    res.json({
+      message: "Signout success",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default {
+  signup,
+  signin,
+  getCurrent,
+  signout,
+};
